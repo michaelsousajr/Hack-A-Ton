2d1ad479126e3bf0f40464274863a0bfc9d5d204
Bryan Tineo <brillant16.gcv@gmail.com>
2023-09-30 14:09:46 -0700

    fixed

--- AuthenticationApp/frontend/src/App.js
+++ AuthenticationApp/frontend/src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import {Routes, Route} from 'react-router-dom';
 import HomePage from './components/HomePage.jsx';
-<<<<<<< HEAD
+
 import Login from './components/LoginPage';
-=======
+
 import LoginPage from './components/LoginPage.jsx';
->>>>>>> a5c729490480c0580da4026a00d4d745884f4149
+
 
 // npm install scrollreveal
 function App() {
     return (
         <Routes>
             <Route path="/" Component={HomePage}/>
-<<<<<<< HEAD
+
             <Route path="/MockLogin" element={<Login />} />
-=======
+
             <Route path="/login" Component={LoginPage}/>
->>>>>>> a5c729490480c0580da4026a00d4d745884f4149
+
         </Routes>
     );
 };
--- AuthenticationApp/frontend/src/components/LoginPage.jsx
+++ AuthenticationApp/frontend/src/components/LoginPage.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
-<<<<<<< HEAD
+
 import { useNavigate } from 'react-router-dom';
 
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
-=======
+
 import '../styles/Login.css'; 
->>>>>>> a5c729490480c0580da4026a00d4d745884f4149
+
 
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -42,99 +42,101 @@ const Login = () => {
   };
 
   return (
-<<<<<<< HEAD
-    <Container component="main" maxWidth="xs">
-      <Typography component="h1" variant="h5">
-        Login
-      </Typography>
-      {error && <Typography color="error">{error}</Typography>}
-      <form onSubmit={handleSubmit}>
-        <TextField
-          variant="outlined"
-          margin="normal"
-          required
-          fullWidth
-          id="email"
-          label="Email Address"
-          name="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <TextField
-          variant="outlined"
-          margin="normal"
-          required
-          fullWidth
-          name="password"
-          label="Password"
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          color="primary"
-        >
-          Login
-        </Button>
-        <button onClick={() => navigate('/')}>Home</button>
 
-      </form>
-    </Container>
-=======
-    <div className="container mt-5">
-      <div className="row justify-content-center">
-        <div className="col-md">
-          <div className="card custom-card">
-            <div className="card-header custom-header">
-              <h2 className="mb-0">Login</h2>
-            </div>
-            <div className="card-body">
-              {error && (
-                <div className="alert alert-danger" role="alert">
-                  {error}
-                </div>
-              )}
-              <form onSubmit={handleSubmit}>
-                <div className="mb-3">
-                  <label htmlFor="email" className="form-label custom-label">
-                    Email Address
-                  </label>
-                  <input
-                    type="email"
-                    className="form-control custom-input"
-                    id="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                  />
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="password" className="form-label custom-label">
-                    Password
-                  </label>
-                  <input
-                    type="password"
-                    className="form-control custom-input"
-                    id="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                  />
-                </div>
-                <button type="submit" className="btn custom-btn btn-block">
-                  Login
-                </button>
-              </form>
+    <>
+      <Container component="main" maxWidth="xs">
+        <Typography component="h1" variant="h5">
+          Login
+        </Typography>
+        {error && <Typography color="error">{error}</Typography>}
+        <form onSubmit={handleSubmit}>
+          <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+          >
+            Login
+          </Button>
+          <button onClick={() => navigate('/')}>Home</button>
+
+        </form>
+      </Container>
+
+      <div className="container mt-5">
+        <div className="row justify-content-center">
+          <div className="col-md">
+            <div className="card custom-card">
+              <div className="card-header custom-header">
+                <h2 className="mb-0">Login</h2>
+              </div>
+              <div className="card-body">
+                {error && (
+                    <div className="alert alert-danger" role="alert">
+                      {error}
+                    </div>
+                )}
+                <form onSubmit={handleSubmit}>
+                  <div className="mb-3">
+                    <label htmlFor="email" className="form-label custom-label">
+                      Email Address
+                    </label>
+                    <input
+                        type="email"
+                        className="form-control custom-input"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="password" className="form-label custom-label">
+                      Password
+                    </label>
+                    <input
+                        type="password"
+                        className="form-control custom-input"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                  </div>
+                  <button type="submit" className="btn custom-btn btn-block">
+                    Login
+                  </button>
+                </form>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
->>>>>>> a5c729490480c0580da4026a00d4d745884f4149
+    </>
+
   );
 };
 
