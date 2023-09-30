f7fba435937953a08ffd31349a0e7c370fe31b9b
Far9999 <sadikalhanssah@gmail.com>
2023-09-30 13:09:03 +0000

    Change workflow of react app
    Organized files and folders to allow for easy navigation and development

--- AuthenticationApp/frontend/src/App.js
+++ AuthenticationApp/frontend/src/App.js
@@ -1,52 +1,14 @@
 import React from 'react';
-import './App.css';
+import {Routes, Route} from 'react-router-dom';
+import HomePage from './components/HomePage.jsx';
+
 
 function App() {
-    /**
-     * Fetches a greeting message related to Open Source learning.
-     *
-     * In essence, when you click the "Learn OpenSource" button in your React app,
-     * this function fetches the greeting message from the Spring Boot backend
-     * and then displays that greeting message in a popup alert in the browser.
-     */
-    const fetchOpenSource = () => {
-        fetch("/api/v1/learn-opensource")       // Makes an HTTP GET request to "/api/v1/learn-opensource"
-            .then(response => response.text())    // Once the response is received, extracts the text from the response.
-            .then(data => {
-                alert(data);                        // Displays an alert with the extracted text data.
-            });
-    }
-
-    /**
-     * Fetches a greeting message related to Cybersecurity learning.
-     *
-     * In essence, when you click the "Learn Cybersecurity" button in your React app,
-     * this function fetches the greeting message from the Spring Boot backend
-     * and then displays that greeting message in a popup alert in the browser.
-     */
-    const fetchCybersecurity = () => {
-        fetch("/api/v1/learn-cybersecurity")    // Makes an HTTP GET request to "/api/v1/learn-cybersecurity"
-            .then(response => response.text())    // Once the response is received, extracts the text from the response.
-            .then(data => {
-                alert(data);                        // Displays an alert with the extracted text data.
-            });
-    }
-
-
-
     return (
-        <div className="container">
-            <div className="header">
-                <h1>Welcome to OpenSource & Cybersecurity Hub</h1>
-                <p>Explore the world of open-source software and dive deep into the essentials of cybersecurity.</p>
-            </div>
-            <div className="content">
-                <button className="button" onClick={() => alert("Welcome to OpenSource!")}>Learn OpenSource</button>
-                <button className="button" onClick={() => alert("Welcome to Cybersecurity!")}>Learn Cybersecurity</button>
-            </div>
-        </div>
+        <Routes>
+            <Route path="/" Component={HomePage}/>
+        </Routes>
     );
-}
+};
 
 export default App;
-
---
+++ AuthenticationApp/frontend/src/components/LoginPage.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const validateForm = () => {
+    if (!email || !password) {
+      setError('All fields are required');
+      return false;
+    }
+    if (!/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(email)) {
+      setError('Invalid email format');
+      return false;
+    }
+    return true;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!validateForm()) {
+      return;
+    }
+
+    // You can put your API calling logic here.
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Typography component="h1" variant="h5">
+        Login
+      </Typography>
+      {error && <Typography color="error">{error}</Typography>}
+      <form onSubmit={handleSubmit}>
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          id="email"
+          label="Email Address"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+        >
+          Login
+        </Button>
+      </form>
+    </Container>
+  );
+};
+
+export default Login;
