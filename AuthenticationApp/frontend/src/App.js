c5953e9a5d337bc6234cba3eb3b7eeb998acb83a
Far9999 <sadikalhanssah@gmail.com>
2023-10-01 03:16:54 +0000

    add and styled first tut page
    All tutorials will be accessed from this page

--- AuthenticationApp/frontend/src/App.js
+++ AuthenticationApp/frontend/src/App.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import {Routes, Route} from 'react-router-dom';
-import HomePage from './components/HomePage.jsx';
-import LoginPage from './components/LoginPage.jsx';
-import SignupPage from './components/SignupPage.jsx';
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage.jsx";
+import LoginPage from "./components/LoginPage.jsx";
+import SignupPage from "./components/SignupPage.jsx";
+import Main from "./components/tuts/Main.jsx";
 
 // npm install scrollreveal
 function App() {
-    return (
-        <Routes>
-            <Route path="/" Component={HomePage}/>
-            <Route path="/login" Component={LoginPage}/>
-            <Route path="/signup" Component={SignupPage}/>
-        </Routes>
-    );
-};
+  return (
+    <Routes>
+      <Route path="/" Component={HomePage} />
+      <Route path="/login" Component={LoginPage} />
+      <Route path="/signup" Component={SignupPage} />
+      <Route path="/tuts/main" Component={Main} />
+    </Routes>
+  );
+}
 
 export default App;
